3670b1beeaa1117782efe0cafad268d08e636a40
Antonio Pérez <apema2079@gmail.com>
2021-11-03 15:18:52 -0600

    Create album struct

--- main.go
+++ main.go
@@ -1,1 +1,9 @@
 package main
+
+// album represents data about a record album
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artits"`
+	Price  float64 `json:"Price"`
+}
