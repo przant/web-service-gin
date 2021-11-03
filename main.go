358e615f10a0b910f841239f9aa2d6d4ef7372bc
Antonio Pérez <apema2079@gmail.com>
2021-11-03 15:42:29 -0600

    Add postAlbums function

--- main.go
+++ main.go
@@ -32,3 +32,18 @@ func main() {
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
+
+// postAlbums adds an album from JSON received in the request body
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// new album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
